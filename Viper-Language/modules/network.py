4c6f236e7ffb107c8651b53b8777c588d6a22547
감성은준 <129740695+Sentimentaleunjun@users.noreply.github.com>
2025-09-27 23:40:58 +0900

    Update network.py

--- Viper-Language/modules/network.py
+++ Viper-Language/modules/network.py
@@ -1,7 +1,4 @@
 import socket
-
 def get_ip(domain):
-    try:
-        return socket.gethostbyname(domain)
-    except:
-        return None
+    try: return socket.gethostbyname(domain)
+    except: return None
