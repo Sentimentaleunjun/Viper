8d9603c3e64ff02e475caa5db04e4c622763584f
감성은준 <129740695+Sentimentaleunjun@users.noreply.github.com>
2025-09-30 22:38:15 +0900

    Update __init__.py

--- viper/__init__.py
+++ viper/__init__.py
@@ -1,1 +1,8 @@
+from .lexer import tokenize
+from .parser import parse
+from .interpreter import eval_ast
 
+def run(code: str):
+    tokens = tokenize(code)
+    ast = parse(tokens)
+    eval_ast(ast)
